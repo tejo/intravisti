6c8a8d3939319d66f857301e9bf4f9c45d000fe1
matteo parmi <m.parmi@gmail.com>
2010-11-07 16:23:48 +0100

    added http auth

--- intravisti.rb
+++ intravisti.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
+require './lib/flickr'
+
+
+use Rack::Auth::Basic do |username, password|
+  [username, password] == ['intra', 'visti']
+end
+
 
 get '/' do
-  "Hello World!"
+  erb :index  
 end
+
+get '/photos.json' do
+  fl = Flickr.new()
+  photos = fl.connection('bruggi')
+  puts photos
+  content_type "application/json"
+  return photos
+end
+
 
--- lib/flickr.rb
+++ lib/flickr.rb
@@ -4,7 +4,7 @@ require 'uri'
 class Flickr
     
   def uri(tags)
-"http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7cb087d5c47cf5b3abc506d250182c60&tags=#{tags}i&tag_mode=all&lang=en-us&format=json"    
+"http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7cb087d5c47cf5b3abc506d250182c60&tags=#{tags}&tag_mode=all&lang=en-us&format=json"    
   end
   def connection(tags)
     Net::HTTP.get URI.parse(uri(tags))
