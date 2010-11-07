9a4b3d4e76ff6ea09324c496f35b0884262cc443
matteo parmi <m.parmi@gmail.com>
2010-11-07 17:52:08 +0100

    introdotto tags consentitiw

--- intravisti.rb
+++ intravisti.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require './lib/flickr'
 
+TAGS = %w(bruggi 3g0ph0t0).freeze
 
 use Rack::Auth::Basic do |username, password|
   [username, password] == ['intra', 'visti']
@@ -11,12 +12,22 @@ get '/' do
   erb :index  
 end
 
-get '/photos.json' do
+get '/photos/:tags' do
+
+  puts params[:tags]
+  error 404, "Photos not found" if has_bad_tags?(params[:tags])
+  
   fl = Flickr.new()
-  photos = fl.connection('bruggi')
-  puts photos
+  photos = fl.connection(params[:tags])
   content_type "application/json"
   return photos
+end
+
+def has_bad_tags?(tags)
+  tags.split('&')[0].split(',').each do |tag|
+    return true unless TAGS.include?(tag)
+  end
+  false
 end
 
 
