74922971bc9c0c217241b65b2280ff9bc1ea9f70
Matteo Parmi <m.parmi@gmail.com>
2010-11-08 23:51:59 +0100

    rimosso supporto jsonp

--- intravisti.rb
+++ intravisti.rb
@@ -12,14 +12,22 @@ get '/' do
   erb :index  
 end
 
-get '/photos/:tags' do
+get '/:tags' do
+  get_photos(params[:tags]) 
+end
 
-  puts params[:tags]
-  error 404, "Photos not found" if has_bad_tags?(params[:tags])
+get '/photos/:tags' do
+  content_type "application/json"
+  get_photos(params[:tags])
+end
+
+def get_photos(tags)
+  puts (tags)
+  error 404, "Photos not found" if has_bad_tags?(tags)
   
   fl = Flickr.new()
-  photos = fl.connection(params[:tags])
-  content_type "application/json"
+  photos = fl.connection(tags)
+  puts photos
   return photos
 end
 
