c820f285216763a52ef64fc52aab58a1a6473b7b
Matteo Parmi <m.parmi@gmail.com>
2010-11-08 13:35:44 +0100

    Aggiunto fix chiodi

--- public/js/ext.js
+++ public/js/ext.js
@@ -13,14 +13,13 @@ var loaded = 0;
 setting.init(0, total);
 for(var i in imgArr) {
 imgList.push($("<img />")
-  .attr("src", imgArr[i])
   .load(function() {
     loaded++;
     setting.loaded(this, loaded, total);
     if(loaded == total) {
     setting.loaded_all(loaded, total);
     }
-    })
+    }).attr("src", imgArr[i])
   );
 }
 
