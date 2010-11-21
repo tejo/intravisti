591b5098404fe21f8ba108fe98a9ede5dd5ce3ca
tejo <m.parmi@gmail.com>
2010-11-21 21:59:38 +0100

    Aggoiunto group id

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -4,7 +4,7 @@ require 'uri'
 class Flickr
     
   def uri(tags)
-"http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7cb087d5c47cf5b3abc506d250182c60&tags=#{tags}&tag_mode=all&lang=en-us&format=json&nojsoncallback=1"    
+"http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7cb087d5c47cf5b3abc506d250182c60&group_id=1571970@N23&tags=#{tags}&tag_mode=all&lang=en-us&format=json&nojsoncallback=1"    
   end
   def connection(tags)
     puts 'flikr hit'
